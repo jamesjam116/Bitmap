ae5e305061a5f82ff3e8d103d80b78fddb5a1b59
yamate0116 <shinydv412@gmail.com>
2023-08-22 00:35:04 +0900

    add mouseleft

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-
 import { Canvas } from '@react-three/fiber'
-import Polyhedron from './Polyhedron'
-import { useState, useEffect } from 'react'
-import * as THREE from 'three'
-import { Stats, OrbitControls } from '@react-three/drei'
-
-
+import { useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import * as THREE from 'three';
 export default function App() {
-  const polyhedron = [
-    new THREE.BoxGeometry(),
-    new THREE.SphereGeometry(0.785398),
-    new THREE.DodecahedronGeometry(0.785398)
-  ]
-  const [isDragging, setIsDragging] = useState(false);
-  const [lastX, setLastX] = useState(null);
-  const [lastY, setLastY] = useState(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
   return (
     <Canvas camera={{ position: [0, 100, 0] }}
       style={{
@@ -24,32 +11,8 @@ export default function App() {
         marginLeft: "10%"
       }}
     >
-      <OrbitControls enableRotate={false} />
-      <mesh
-        onPointerDown={(e) => {
-          setIsDragging(true);
-          setLastX(e.clientX);
-          setLastY(e.clientY);
-        }}
-        onPointerUp={(e) => {
-          setIsDragging(false);
-
-        }}
-        onPointerMove={(e) => {
-          if (isDragging) {
-            const deltaX = e.clientX - lastX;
-            const deltaY = e.clientY - lastY;
-            setPosition((prevPosition) => ({
-              x: prevPosition.x + deltaX,
-              y: prevPosition.y + deltaY,
-            }));
-            setLastX(e.clientX);
-            setLastY(e.clientY);
-            console.log(position)
-          }
-        }}
-
-      >
+      <OrbitControls enableRotate={false} mouseButtons={{ LEFT: THREE.MOUSE.PAN }} />
+      <mesh>
         <gridHelper args={[100, 100]} material={new THREE.LineBasicMaterial({ color: 'white' })} backgroundColor="#3b71db" />
       </mesh>
     </Canvas >
