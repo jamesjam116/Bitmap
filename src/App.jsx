833c68e5e0a8f424b40d84d1ebeff77a4eeefe6c
yamate0116 <shinydv412@gmail.com>
2023-08-23 03:12:57 +0900

    fix fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,117 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber'
 import { useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import * as THREE from 'three';
-import NFT from './components/NFT';
-import { Scene } from 'three';
-import Polyhedron from './Polyhedron';
 import { useThree } from '@react-three/fiber';
-import Sphere from "./Sphere.Component";
 
 function GridCell(props) {
   const ref = useRef();
-  const handleClick = async (event) => {
+  let bitmap;
+  const clickevent = async () => {
+    const position = ref.current.position;
+
+  }
+  const fetchNFT = async () => {
     const position = ref.current.position;
     const cell = [position.x, position.z];
+    // await fetch(`https://global.oasis.world/service/game/bitmap/detail?bitNumber=${(position.z) * 10 + position.x}`)
+    //   .then(async (res) => {
+    //     let data = await res.json();
+    //     console.log(position, data.data.contentUrl)
+    //     bitmap = data.data.contentUrl;
+    //     return data;
+    //   })
+    //   .then((url) => {
+    //     return new Promise((resolve, reject) => {
+    //       fetch(url)
+    //         .then((res) => {
+    //           resolve(res);
+    //         })
+    //         .catch((err) => {
+    //           reject(err);
+    //         });
+    //     });
+    //   })
+    //   .then((res) => {
+    //   })
+    //   .catch((err) => {
+    //   });
 
-    await fetch("https://global.oasis.world/service/game/bitmap/detail?bitNumber=0")
-      .then(async (res) => {
-        let data = await res.json();
-        console.log(data.data.contentUrl)
-        return data;
-      })
-      .then((url) => {
-        return new Promise((resolve, reject) => {
-          fetch(url)
-            .then((res) => {
-              console.log(res);
-              resolve(res);
-            })
-            .catch((err) => {
-              console.log(err)
-              reject(err);
-            });
-        });
-      })
-      .then((res) => {
-        // do something with the second response
-        console.log(res, "------------->")
-      })
-      .catch((err) => {
-        console.error(err);
-      });
 
+    // https://ordinals.com/content/c52d5be04fc1cbb3c3f5f8f5be1d70e5afc7d8699e18d088f5d192e55f8191f5i0
     // Add your code to create an image on the clicked cell
-    const texture = new THREE.TextureLoader().load('https://ordinals.com/content/c52d5be04fc1cbb3c3f5f8f5be1d70e5afc7d8699e18d088f5d192e55f8191f5i0');
+    let texture;
+    // if (bitmap == undefined) {
+    // texture = new THREE.TextureLoader().load('/img/avatar.png');
+    // } else {
+    // texture = new THREE.TextureLoader().load('https://ordinals' + bitmap.slice(16))
+    // }
     // Create a material from the texture
+    texture = new THREE.TextureLoader().load('/img/avatar.png');
     const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
     // Create a mesh with the material and position it on the clicked cell
     const mesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
     mesh.position.x = position.x;
     mesh.position.y = 0.1; // Set the y position slightly above the grid
     mesh.position.z = position.z;
+    mesh.rotation.x = -Math.PI / 2;
     // Face the image towards the camera
-    mesh.quaternion.copy(event.camera.quaternion); // Set the mesh's quaternion to the camera's quaternion
     // Add the mesh to the scene
     ref.current.parent.add(mesh);
   };
+  useEffect(() => {
+    // fetchNFT();
+  }, [])
   return (
-    <mesh ref={ref} onClick={handleClick} position={[props.x, 0, props.y]}>
+    <mesh ref={ref} onClick={clickevent} position={[props.x, 0, props.y]}>
       <boxBufferGeometry attach="geometry" args={[0.9, 0.1, 0.9]} />
     </mesh>
   );
 }
 
+// function Grid(props) {
+//   const cells = [];
+//   for (let x = 0; x < props.size.x; x++) {
+//     for (let y = 0; y < props.size.y; y++) {
+//       cells.push(<GridCell key={`${x},${y}`} x={x} y={y} />);
+//     }
+//   }
+//   return <>{cells}</>;
+// }
+
 function Grid(props) {
   const cells = [];
-  for (let x = 0; x < props.size; x++) {
-    for (let y = 0; y < props.size; y++) {
+  const lines = [];
+  for (let x = 0; x < props.size.x; x++) {
+    for (let y = 0; y < props.size.y; y++) {
       cells.push(<GridCell key={`${x},${y}`} x={x} y={y} />);
+      if (x < props.size.x - 1) {
+        lines.push(
+          <mesh key={`x${x},${y}`} position={[x + 0.5, 0, y]}>
+            <boxBufferGeometry attach="geometry" args={[1, 0.2, 0.05]} />
+            {/* <meshBasicMaterial attach="material" color={0xff0000} /> */}
+          </mesh>
+        );
+      }
+      if (y < props.size.y - 1) {
+        lines.push(
+          <mesh key={`y${x},${y}`} position={[x, 0, y + 0.5]}>
+            <boxBufferGeometry attach="geometry" args={[0.05, 0.2, 1]} />
+            {/* <meshBasicMaterial attach="material" color={0xff0000} /> */}
+          </mesh>
+        );
+      }
     }
   }
-  return <>{cells}</>;
+  return (
+    <>
+      {cells}
+      {lines}
+    </>
+  );
 }
+
 function CameraControls() {
   const { camera, gl } = useThree();
 
@@ -94,7 +134,7 @@ export default function App() {
       }}
     >
       <CameraControls />
-      <Grid size={10} />
+      <Grid size={{ x: 800, y: 100 }} />
     </Canvas>
   )
 }
